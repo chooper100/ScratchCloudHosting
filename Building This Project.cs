b740af708a4c2712f0a8c22e7d1774adf2f000ea
Chooper100 <chooper100.scratch@gmail.com>
2015-10-28 15:20:20 +0000

    Create Building This Project.cs

---
+++ Building This Project.cs
@@ -0,0 +1,7 @@
+I am using Visual Studio 2015 Enterprise to build this project.
+
+At the moment, it is standard console application.
+
+Currently, I am using the Mircosoft.Net frameword 4.6 when compiling and will consider dropping down to a lower framework version once I have some stable code running.
+
+So yeah :)
