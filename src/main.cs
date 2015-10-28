057d6a1cc748fc6e8091e2de5027a5bced87546e
Chooper100 <chooper100.scratch@gmail.com>
2015-10-28 15:47:16 +0000

    Removed constants

--- src/main.cs
+++ src/main.cs
@@ -10,13 +10,6 @@ namespace Scratch_Cloud
 {
     class Program
     {
-
-        const string SERVER = "scratch.mit.edu";
-        const string PROJECTS_SERVER = "projects.scratch.mit.edu";
-        const string CDN_SERVER = "cdn.scratch.mit.edu";
-        const string CLOUD_SERVER = "cloud.scratch.mit.edu";
-        const int CLOUD_SERVER_PORT = 531;
-
         static void Main(string[] args)
         {
             Console.WriteLine("Please enter your username:");
