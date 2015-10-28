d9455dc2ca454e31b82caa4ad22e2a3b19edf562
Chooper100 <chooper100.scratch@gmail.com>
2015-10-28 17:31:43 +0000

    Comment Code

--- src/main.cs
+++ src/main.cs
@@ -11,79 +11,101 @@ namespace Scratch_Cloud
     {
         static void Main(string[] args)
         {
-            Title("Please enter your username:", true);
+            //Get user input
+
+            Heading("Please enter your username:", true);
             string uname = Console.ReadLine();
 
-            Title("Please enter your password:", true);
+            Heading("Please enter your password:", true);
             string pword = Console.ReadLine();
 
             Console.WriteLine("");
-            Title("Attempting Logon...", true);
+            Heading("Attempting Logon...", true);
+
+            //Prepare login request
 
             HttpWebRequest login = (HttpWebRequest)WebRequest.Create("https://scratch.mit.edu/login/");
-            login.Referer = "https://scratch.mit.edu";
+            login.Referer = "https://scratch.mit.edu"; // Required by Scratch servers
             login.Headers.Add("Cookie", "scratchcsrftoken=a; scratchlanguage=en;");
             login.Headers.Add("X-CSRFToken", "a");
             login.Headers.Add("X-Requested-With", "XMLHttpRequest");
             login.Host = "scratch.mit.edu";
             login.Method = "POST";
             
+            //Generate post data (JSON object with properties username and password)
             string postData = JsonConvert.SerializeObject(new UserInput(uname, pword));
 
-            Title("Post Data: ", false);
+            Heading("Post Data: ", false);
             Console.WriteLine(postData);
 
+            //Encode post data as byte array
             byte[] byteData = Encoding.UTF8.GetBytes(postData);
 
             login.ContentType = "application/x-www-form-urlencoded";
             login.ContentLength = byteData.Length;
 
+            //Write post data to request data stream
             Stream dataStream = login.GetRequestStream();
             dataStream.Write(byteData, 0, byteData.Length);
             dataStream.Close();
 
+            //Get request response
             HttpWebResponse response = (HttpWebResponse)login.GetResponse();
 
-            Title("Response: ", false);
+            Heading("Response: ", false);
             Console.WriteLine(response.StatusDescription);
 
             Console.WriteLine("");
-            Title("User Data: ", true);
+            Heading("User Data: ", true);
 
+            //Get session id
             string sessionid = "";
             string[] parts = response.Headers["Set-Cookie"].Split(";"[0]);
-            for (int i = 0; i < parts.Length; i++)
+            for (int i = 0; i < parts.Length; i++) //For each cookie
             {
-                string cookie = parts[i].TrimStart(" "[0]);
-                string[] cookieParts = cookie.Split("="[0]);
-                if (cookieParts[0] == "scratchsessionsid")
+                string cookie = parts[i].TrimStart(" "[0]); //Remove leading spaces
+                string[] cookieParts = cookie.Split("="[0]); //Split by equals sine
+                if (cookieParts[0] == "scratchsessionsid") //If cookie contains the session id
                 {
-                    sessionid = cookieParts[1];
+                    sessionid = cookieParts[1]; //Set sessionid to the session id
                 }
             }
 
+            //Read response data
             Stream responseStream = response.GetResponseStream();
             StreamReader reader = new StreamReader(responseStream);
+
+            //Deserialize JSON object
             JArray bodyData = (JArray)JsonConvert.DeserializeObject(reader.ReadToEnd());
 
+            //Create user
             User user = new User(bodyData[0]["username"].ToString(),(int) bodyData[0]["id"], sessionid);
             
-            Title("Username: ", false);
+            //Output user info
+            Heading("Username: ", false);
             Console.WriteLine(user.Username);
 
-            Title("ID: ", false);
+            Heading("ID: ", false);
             Console.WriteLine(user.ID);
 
-            Title("Session ID: ", false);
+            Heading("Session ID: ", false);
             Console.WriteLine(user.SessionID);
 
+            //Close all streams
             reader.Close();
             responseStream.Close();
             response.Close();
+
+            //Wait for enter key press
             Console.Read();
         }
         
-        private static void Title(string message, bool newLine)
+        /// <summary>
+        /// Outputs a heading to the console.
+        /// </summary>
+        /// <param name="message">The message of the heading.</param>
+        /// <param name="newLine">Indicates whether to append a new line to the end of the message.</param>
+        private static void Heading(string message, bool newLine)
         {
             Console.ForegroundColor = ConsoleColor.Cyan;
             if (newLine)
@@ -96,30 +118,36 @@ namespace Scratch_Cloud
             Console.ForegroundColor = ConsoleColor.Gray;
         }
     }
-}
 
-struct UserInput
-{
-    public string username;
-    public string password;
-
-    public UserInput(string uname, string pword)
+    /// <summary>
+    /// Used for encoding login request data
+    /// </summary>
+    struct UserInput
     {
-        username = uname;
-        password = pword;
+        public string username;
+        public string password;
+
+        public UserInput(string uname, string pword)
+        {
+            username = uname;
+            password = pword;
+        }
     }
-}
 
-struct User
-{
-    public string Username;
-    public int ID;
-    public string SessionID;
-
-    public User(string username, int id, string sessionid)
+    /// <summary>
+    /// Contains user info
+    /// </summary>
+    struct User
     {
-        Username = username;
-        ID = id;
-        SessionID = sessionid;
+        public string Username;
+        public int ID;
+        public string SessionID;
+
+        public User(string username, int id, string sessionid)
+        {
+            Username = username;
+            ID = id;
+            SessionID = sessionid;
+        }
     }
 }
