a4e99b52c3223563a68dfca9fa029830f44806d3
Chooper100 <chooper100.scratch@gmail.com>
2015-10-28 15:45:25 +0000

    This might actually work...

--- src/main.cs
+++ src/main.cs
@@ -19,7 +19,41 @@ namespace Scratch_Cloud
 
         static void Main(string[] args)
         {
+            Console.WriteLine("Please enter your username:");
+            string uname = Console.ReadLine();
+
+            Console.WriteLine("Please enter your password:");
+            string pword = Console.ReadLine();
+
+            Console.WriteLine("");
+            Console.WriteLine("Attempting Logon...");
+
+            HttpWebRequest login = (HttpWebRequest)WebRequest.Create("https://scratch.mit.edu/login/");
+            login.Referer = "https://scratch.mit.edu";
+            login.Headers.Add("Cookie", "scratchcsrftoken=a; scratchlanguage=en;");
+            login.Headers.Add("X-CSRFToken", "a");
+            login.Headers.Add("X-Requested-With", "XMLHttpRequest");
+            login.Host = "scratch.mit.edu";
+            login.Method = "POST";
+
+            //Will improve this eventually to avoid SQL injection type inputs
+            string postData = "{\"username\":\"" + uname + "\", \"password\":\"" + pword + "\"}";
+
+            Console.WriteLine("Post data: " + postData);
+
+            byte[] byteData = Encoding.UTF8.GetBytes(postData);
+
+            login.ContentType = "application/x-www-form-urlencoded";
+            login.ContentLength = byteData.Length;
+
+            Stream dataStream = login.GetRequestStream();
+            dataStream.Write(byteData, 0, byteData.Length);
+            dataStream.Close();
+
+            HttpWebResponse response = (HttpWebResponse)login.GetResponse();
+            Console.WriteLine("Response: " + response.StatusDescription);
             
+            Console.Read();
         }
     }
 }
